bfe2fdcacf38b8f534d0f8b2b4fe2ba6bfa452ee
chetran <114576865+chetran@users.noreply.github.com>
2022-12-06 15:16:52 +0100

    Add files via upload

---
+++ day 6/solution.py
@@ -0,0 +1,22 @@
+import re 
+
+First_markerer_packet = int() 
+First_markerer_message = int() 
+with open("input.txt", "r") as file:
+    for seq in file:
+        #Part 1
+        for index in range(3,len(seq)):
+            Start_of_packet = set(re.findall(r"\w",seq[index - 3: index + 1]))
+            if len(Start_of_packet) == 4:
+                First_markerer_packet = index + 1
+                break
+        #Part 2
+        for index in range(13,len(seq)):
+            Start_of_message = set(re.findall(r"\w",seq[index - 13: index + 1]))
+            if len(Start_of_message) == 14:
+                First_markerer_message = index + 1
+                break
+            
+
+print("Characters needed to be processed before the first start-of-packet marker is detected:", First_markerer_packet)
+print("Characters needed to be processed before the first start-of-message marker is detected:", First_markerer_message)
